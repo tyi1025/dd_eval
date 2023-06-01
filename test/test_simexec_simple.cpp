e54c574221ed4ea6d5c09750b83794f8256376e6
Tianyi Wang <tianyi1.wang@tum.de>
2023-06-01 21:59:05 +0200

    Move non-parameterized simple test case to a separate file

---
+++ test/test_simexec_simple.cpp
@@ -0,0 +1,19 @@
+#include "CircuitSimulator.hpp"
+#include "CircuitSimulatorExecutor.hpp"
+#include "SimulationTask.hpp"
+
+#include <gtest/gtest.h>
+#include <memory>
+
+TEST(DDSIMExecSimpleTest, EmptyCircuit) {
+  auto qc               = std::make_unique<qc::QuantumComputation>(2U);
+  auto clonedQC         = std::make_unique<qc::QuantumComputation>(qc->clone());
+  auto simulationTask   = std::make_unique<SimulationTask>(std::move(clonedQC));
+  auto circuitSimulator = std::make_unique<CircuitSimulator<>>(std::move(qc));
+  auto circuitSimulatorExecutor = std::make_unique<CircuitSimulatorExecutor>();
+  circuitSimulatorExecutor->setCircuitSimulator(circuitSimulator);
+  circuitSimulatorExecutor->setMSimTask(simulationTask);
+  json const result = circuitSimulatorExecutor->executeTask();
+
+  EXPECT_EQ(1, result["00"]);
+}
