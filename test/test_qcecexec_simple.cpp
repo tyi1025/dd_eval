f80e730a5f0bea4023ac2795d6a5327cf245c9b4
Tianyi Wang <tianyi1.wang@tum.de>
2023-06-03 00:50:59 +0200

    Refactor verification tests to avoid empty circuits

--- test/test_qcecexec_simple.cpp
+++ test/test_qcecexec_simple.cpp
@@ -4,9 +4,15 @@
 #include <gtest/gtest.h>
 #include <memory>
 
-TEST(QCECExecSimpleTest, EmptyCircuits) {
+TEST(QCECExecSimpleTest, SimpleCircuit) {
   auto qc1 = std::make_unique<qc::QuantumComputation>(2U);
+  qc1->x(0U);
+  qc1->x(1U);
   auto qc2 = std::make_unique<qc::QuantumComputation>(2U);
+  qc2->x(0U);
+  qc2->x(0U);
+  qc2->x(0U);
+  qc2->x(1U);
   auto verificationTask =
       std::make_unique<VerificationTask>(std::move(qc1), std::move(qc2));
   auto alternatingVerificationExecutor =
