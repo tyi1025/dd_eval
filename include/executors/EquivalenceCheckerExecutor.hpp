39ca3e250126e5806f61e19f4fe2c55c00608c60
Tianyi Wang <tianyi1.wang@tum.de>
2023-07-14 17:08:28 +0200

    Add tests for qcec execs

--- include/executors/EquivalenceCheckerExecutor.hpp
+++ include/executors/EquivalenceCheckerExecutor.hpp
@@ -10,17 +10,34 @@ public:
   [[nodiscard]] std::string getIdentifier() const override {
     return "equivalence_checker";
   }
+
+private:
   bool runConstructionChecker = false;
   bool runSimulationChecker   = false;
   bool runAlternatingChecker  = true;
 
-  void setRunConstructionChecker(bool run) {
+public:
+  [[maybe_unused]] [[nodiscard]] bool isRunConstructionChecker() const {
+    return runConstructionChecker;
+  };
+
+  [[maybe_unused]] [[nodiscard]] bool isRunSimulationChecker() const {
+    return runSimulationChecker;
+  };
+
+  [[maybe_unused]] [[nodiscard]] bool isRunAlternatingChecker() const {
+    return runAlternatingChecker;
+  };
+
+  [[maybe_unused]] void setRunConstructionChecker(bool run) {
     this->runConstructionChecker = run;
   };
 
-  void setRunSimulationChecker(bool run) { this->runSimulationChecker = run; };
+  [[maybe_unused]] void setRunSimulationChecker(bool run) {
+    this->runSimulationChecker = run;
+  };
 
-  void setRunAlternatingChecker(bool run) {
+  [[maybe_unused]] void setRunAlternatingChecker(bool run) {
     this->runAlternatingChecker = run;
   };
 
@@ -30,29 +47,51 @@ public:
     this->runAlternatingChecker  = false;
   };
 
+private:
   bool sequentialApplicationScheme   = false;
   bool oneToOneApplicationScheme     = false;
   bool proportionalApplicationScheme = true;
   bool lookaheadApplicationScheme    = false;
   bool gateCostApplicationScheme     = false;
 
-  void setSequentialApplicationScheme(bool ac) {
+public:
+  [[maybe_unused]] [[nodiscard]] bool isSequentialApplicationScheme() const {
+    return sequentialApplicationScheme;
+  }
+
+  [[maybe_unused]] [[nodiscard]] bool isOneToOneApplicationScheme() const {
+    return oneToOneApplicationScheme;
+  }
+
+  [[maybe_unused]] [[nodiscard]] bool isProportionalApplicationScheme() const {
+    return proportionalApplicationScheme;
+  }
+
+  [[maybe_unused]] [[nodiscard]] bool isLookaheadApplicationScheme() const {
+    return lookaheadApplicationScheme;
+  }
+
+  [[maybe_unused]] [[nodiscard]] bool isGateCostApplicationScheme() const {
+    return gateCostApplicationScheme;
+  }
+
+  [[maybe_unused]] void setSequentialApplicationScheme(bool ac) {
     this->sequentialApplicationScheme = ac;
   };
 
-  void setOneToOneApplicationScheme(bool ac) {
+  [[maybe_unused]] void setOneToOneApplicationScheme(bool ac) {
     this->oneToOneApplicationScheme = ac;
   };
 
-  void setProportionalApplicationScheme(bool ac) {
+  [[maybe_unused]] void setProportionalApplicationScheme(bool ac) {
     this->proportionalApplicationScheme = ac;
   };
 
-  void setLookaheadApplicationScheme(bool ac) {
+  [[maybe_unused]] void setLookaheadApplicationScheme(bool ac) {
     this->lookaheadApplicationScheme = ac;
   };
 
-  void setGateCostApplicationScheme(bool ac) {
+  [[maybe_unused]] void setGateCostApplicationScheme(bool ac) {
     this->gateCostApplicationScheme = ac;
   };
 
